1ed954ef8ad2d866b7308fdd6e0cadd46873efda
drakeerv <drakeerv@outlook.com>
2023-03-06 22:57:22 -0500

    Major Rework to use robot_py apriltags (1/2 commit)

--- source/finder.py
+++ source/finder.py
@@ -1,20 +1,26 @@
 from config import load_config
 from numpy import ndarray, array
-from pupil_apriltags import Detector, Detection
 from shared_memory_dict import SharedMemoryDict
 from time import time, sleep
 from platform_constants import cv2_backend
+from wpimath.geometry import Pose3d, Rotation3d, Translation3d
 from viewer.webmjpeg import MjpegViewer
 from viewer.draw import draw
-from pytransform3d import transformations
+from math import pi, degrees
 
+import robotpy_apriltag as apriltag
 import cv2
 
 # Load the config files
-ENVIROMENT = load_config("enviroment")
 CONFIG = load_config("config")
+FIELD = CONFIG["config"]["field"]
 
-used_ids = set(ENVIROMENT["tags"].keys())
+FIELD_TAGS = load_config("fields/" + FIELD + "-tag.json")
+DECISION_MARGIN = FIELD_TAGS["decision_margin"]
+DETECTION_ITERATIONS = FIELD_TAGS["detection_iterations"]
+
+FIELD_LAYOUT = load_config("fields/" + FIELD + ".json")
+
 
 class Finder:
     def __init__(self, camera_config: dict, shared_dict: SharedMemoryDict) -> None:
@@ -24,46 +30,104 @@ class Finder:
         self.shared_dict = shared_dict
 
         self.stream = cv2.VideoCapture(self.camera_config["port"], cv2_backend)
-        self.calibration = load_config("calibrations/" + self.camera_config["type"])
+        self.calibration = load_config(
+            "calibrations/" + self.camera_config["type"])
 
-        self.detector = Detector(
-            **self.calibration["detector"],
-            **{"families": ENVIROMENT["tag_family"], "debug": 0}
-        )
-        
+        self.detector = apriltag.AprilTagDetector()
+        self.detector.addFamily(FIELD_TAGS["family"])
+
         self.camera_matrix = array(self.calibration["camera_matrix"])
-        self.camera_params = (
-            self.camera_matrix[0, 0],
-            self.camera_matrix[1, 1],
-            self.camera_matrix[0, 2],
-            self.camera_matrix[1, 2]
+        self.estimator = apriltag.AprilTagPoseEstimator(
+            apriltag.AprilTagPoseEstimator.Config(
+                # 0.2, 500, 500, frame_size[1] / 2.0, frame_size[0] / 2.0 # orig not for HD3000, might be picam?
+                FIELD_TAGS["size"], *(self.camera_matrix[0, 0], self.camera_matrix[1, 1], self.camera_matrix[0,
+                                      2], self.camera_matrix[1, 2])  # from wpiilbjExamples HD3000 config values
+            )
         )
+
+        self.field = apriltag.AprilTagFieldLayout(
+            FIELD["tags"], FIELD["field"]["length"], FIELD["field"]["width"])
 
         self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, self.camera_config["width"])
-        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, self.camera_config["height"])
+        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT,
+                        self.camera_config["height"])
         self.stream.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
         self.stream.set(cv2.CAP_PROP_FPS, self.camera_config["fps"])
 
         if CONFIG["features"]["webmjpeg_viewer"]["enabled"]:
-            self.mjpeg = MjpegViewer(int(self.camera_config["port"]), int(CONFIG["features"]["webmjpeg_viewer"]["starting_port"]))
+            self.mjpeg = MjpegViewer(int(self.camera_config["port"]), int(
+                CONFIG["features"]["webmjpeg_viewer"]["starting_port"]))
             self.mjpeg.start()
         else:
             self.mjpeg = None
 
         self.previous_time = time()
 
+    def process_apriltag(self, tag):
+        est = self.estimator.estimateOrthogonalIteration(
+            tag, DETECTION_ITERATIONS)
+        return {"tag": tag, "estimate": est.pose1}
+
     def find(self, frame: ndarray) -> list[dict]:
         """Find the tags in the frame."""
 
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        tags = self.detector.detect(
-            frame, estimate_tag_pose=True, camera_params=self.camera_params, tag_size=ENVIROMENT["tag_size"])
-        return tags
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        tags = self.remove_errors(self.detector.detect(gray))
+        poses = [self.process_apriltag(tag) for tag in tags]
 
-    def remove_errors(self, tags: list[Detection]) -> None:
-        """Remove the tags that are not in the enviroment, have a high hamming distance, or pose error."""
+        return poses
 
-        return [tag for tag in tags if str(tag.tag_id) in used_ids and tag.tag_family.decode() == ENVIROMENT["tag_family"] and tag.hamming <= 1 and tag.pose_err <= 0.0001]
+    def solve(self, poses: list[dict]) -> list[dict]:
+        """Solves for the position of the robot on the field. It is weighted by the decision margin of the tag."""
+
+        transformations = []
+        for pose in poses:
+            tag = pose["tag"]
+            tag_estimate = pose["estimate"]
+
+            known_pose = self.field.getTagPose(tag.getId())
+
+            tag_estimate_pose_rotation = tag_estimate.rotation()
+            tag_estimate_pose_translation = tag_estimate.translation()
+
+            tag_estimate_pose = Pose3d(
+                tag_estimate_pose_rotation, tag_estimate_pose_translation)
+            camera_pose = self.tagposeToCameraPosition(
+                tag_estimate_pose, known_pose)
+
+    def tagposeToCameraPosition(self, tag_estimate_pose, known_pose):
+        tag_atc_rel_pose = tag_estimate_pose
+        cam_atc_rel_rot = -tag_atc_rel_pose.rotation()
+        cam_atc_rel_transl = -tag_atc_rel_pose.translation()
+        cam_atc_rel_pose = Pose3d(cam_atc_rel_transl, cam_atc_rel_rot)
+        temppose = cam_atc_rel_pose.relativeTo(tag_atc_rel_pose)
+        x = -temppose.Z()/2.0
+        y = temppose.X()/2.0
+        z = -temppose.Y()/2.0
+        cameraZangle = known_pose.rotation().Z() + pi - cam_atc_rel_rot.Y()
+        # cam_fcs_rel = Pose3d(Translation3d(x, y, z),
+        #                      Rotation3d(0, 0, cameraZangle))
+        cam_fcs_abs = Pose3d(Translation3d(x+known_pose.X(), y+known_pose.Y(),
+                             z+known_pose.Z()), Rotation3d(0, 0, cameraZangle))
+        # xr, yr, zr = cam_fcs_rel.translation().X(
+        # ), cam_fcs_rel.translation().Y(), cam_fcs_rel.translation().Z()
+        xa, ya, za = cam_fcs_abs.translation().X(
+        ), cam_fcs_abs.translation().Y(), cam_fcs_abs.translation().Z()
+        cameraZangleDeg = degrees(cameraZangle)
+        
+        return {
+            "heading": cameraZangleDeg,
+            "absolute": {
+                "x": xa,
+                "y": ya,
+                "z": za
+            }
+        }
+
+    def remove_errors(self, tags: list[apriltag.AprilTagDetection]) -> None:
+        """Remove the tags that are not in the ENVIRONMENT, have a high hamming distance, or pose error."""
+
+        return [tag for tag in tags if tag.getDecisionMargin() > DECISION_MARGIN]
 
     def run(self) -> None:
         """Run the finder in a loop."""
@@ -83,8 +147,10 @@ class Finder:
                     draw(frame, found_tags)
                 self.mjpeg.update_frame(frame)
 
-            self.shared_dict["tags"] = {tag.tag_id: transformations.transform_from(array(tag.pose_R), array(tag.pose_t).flatten()) for tag in found_tags}
+            self.shared_dict["tags"] = {tag.tag_id: transformations.transform_from(
+                array(tag.pose_R), array(tag.pose_t).flatten()) for tag in found_tags}
             self.previous_time = time()
+
 
 def start_finder_process(camera_port: int, camera_config: dict, share_settings: dict) -> None:
     """Start the finder process."""
--- source/solver.py
+++ source/solver.py
@@ -1,65 +1,26 @@
 from config import load_config
-from pytransform3d import rotations
-from pytransform3d import transformations
-from pytransform3d import transform_manager
 from numpy import array
-from math import degrees
-import matplotlib.pyplot as plt
 
 # Load the config files
-ENVIROMENT = load_config("enviroment")
+ENVIRONMENT = load_config("environment")
 CAMERAS = load_config("cameras")
 
-
 def solve(data: dict[list]) -> dict:
-    tm = transform_manager.TransformManager()
-
+    transformations = []
+    
     for camera_port, detections in data.items():
         camera = CAMERAS[camera_port]
         robot_to_camera = array(camera["transformation"])
-        camera_to_robot = transformations.invert_transform(robot_to_camera)
-
-        tm.add_transform("robot", f"camera-{camera_port}", robot_to_camera)
 
         for tag_id, tag_transformation in detections.items():
-            tag_data = ENVIROMENT["tags"][str(tag_id)]
-            tag_size = ENVIROMENT["tag_size"]
+            tag_data = ENVIRONMENT["tags"][str(tag_id)]
+            tag_size = ENVIRONMENT["tag_size"]
 
             field_to_tag = array(tag_data["transformation"]).reshape(4, 4)
             camera_to_tag = array(tag_transformation).reshape(4, 4)
+            camera_to_tag *= tag_size
 
-            try:
-                print(camera_to_tag[:3, 3])
-                camera_to_tag[:3, 3] /= tag_size
-                camera_to_tag[:3, 3] = [camera_to_tag[2, 3], -camera_to_tag[0, 3], camera_to_tag[1, 3]]
-                # camera_to_tag[:3, 3] = [0, 0, 0]
-                # print(camera_to_tag[:3, 3])
+            print(camera_to_tag[:3, 3])
 
-                camera_to_tag_rotation = rotations.compact_axis_angle_from_matrix(camera_to_tag[:3, :3])
-                camera_to_tag_rotation = array([camera_to_tag_rotation[2], camera_to_tag_rotation[0], camera_to_tag_rotation[1]])
-                camera_to_tag[:3, :3] = rotations.matrix_from_compact_axis_angle(camera_to_tag_rotation)
-                # camera_to_tag[:3, :3] = rotations.matrix_from_compact_axis_angle([0, 0, 0])
-
-                tm.add_transform(f"camera-{camera_port}", f"tag-{tag_id}", camera_to_tag)
-                tm.add_transform("field", f"tag-{tag_id}", field_to_tag)
-            except:
-                continue
-
-    if not tm.has_frame("field"):
-        return None
-    
-    average_transformation = tm.get_transform("field", "robot")
-
-    ax = tm.plot_frames_in("field", s=1)
-    ax.set_xlim(-7, 7)
-    ax.set_ylim(-7, 7)
-    ax.set_zlim(-7, 7)
-    plt.show()
-
-    # convert the transformation to a dictionary
-    return {
-        "position": average_transformation[:3, 3],
-        "rotation": [degrees(axis) for axis in rotations.compact_axis_angle_from_matrix(average_transformation[:3, :3])],
-        "transformation": average_transformation,
-        "manager": tm,
-    }
+    if len(transformations) == 0:
+        return None 
